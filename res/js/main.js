fcc904c6037eb2e3d6d034d607ea51a2b7f8143b
StanLee <tangjianyi1993@live.com>
2016-08-05 19:54:02 +0800

    advance edtail
    细节稍作修改

--- res/js/main.js
+++ res/js/main.js
@@ -18,15 +18,6 @@ $('a[data-toggle="tooltip"]').bind('click', function() {
 	$('#contactModal').modal('hide');
 })
 
-/* toggle time roundabout */
-//TODO
-$('#showTime').on('mouseover', function() {
-	console.log('show time!');
-})
-.on('mouseout', function() {
-	console.log('hide time!');
-})
-
 $('#blogLink').on('click', function() {
 	if (!$('#bodyContainer').hasClass('init')) {
 		papers.initCategory(category);
